a70c72d9d58de3a9d47577085f772e3906b1c8cd
JennieGB <jennie4study@gmail.com>
2024-04-03 23:11:57 +0300

    Homework

---
+++ React/Homework/2/list.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CommentsList from './CommentsList';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <CommentsList />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
